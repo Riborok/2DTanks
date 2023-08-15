3c20178b3326d8e7b8854c9730d4dd1cfb27a3bb
Ferret <stas.gabrus@gmail.com>
2023-08-15 17:57:04 +0300

    added stats for standard parts of tank

--- src/ts/model/tank/ITurret.ts
+++ src/ts/model/tank/ITurret.ts
@@ -5,3 +5,12 @@ export interface ITurret {
 
     get bulletCapacity(): number;
 }
+
+class TurretModel0 implements ITurret{
+    private _angle: number = 0;
+    private static readonly ANGLE_SPEED: number = 0.05;
+    get angle(): number {return this._angle};
+    get bulletCapacity(): number {return 10};
+    clockwiseMovement(): void {this._angle -= TurretModel0.ANGLE_SPEED};
+    counterclockwiseMovement(): void {this._angle += TurretModel0.ANGLE_SPEED};
+}
