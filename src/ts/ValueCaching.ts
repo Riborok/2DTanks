54392a522fc07cf4b63cf38edbd359920d5afa92
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-08-15 20:34:01 +0300

    minor updates

--- src/ts/ValueCaching.ts
+++ src/ts/ValueCaching.ts
@@ -1,18 +1,18 @@
 class ValueCaching {
-    private readonly cache: Map<number, number> = new Map();
+    private readonly _cache: Map<number, number> = new Map();
     private readonly _calcFunc: (index: number) => number;
     private static readonly MAX_CACHE_SIZE: number = 42;
 
     public constructor(calcFunc: (index: number) => number) { this._calcFunc = calcFunc; }
     public getValue(index: number): number {
-        if (this.cache.has(index))
-            return this.cache.get(index);
+        if (this._cache.has(index))
+            return this._cache.get(index);
 
-        if (this.cache.size > ValueCaching.MAX_CACHE_SIZE)
-            this.cache.clear();
+        if (this._cache.size > ValueCaching.MAX_CACHE_SIZE)
+            this._cache.clear();
 
         const value = this._calcFunc(index);
-        this.cache.set(index, value);
+        this._cache.set(index, value);
 
         return value;
     }
--- src/ts/model/tank/IWeapon.ts
+++ src/ts/model/tank/IWeapon.ts
@@ -8,10 +8,10 @@ export interface IWeapon {
     get barrelLength(): number;
 }
 
-class WeaponModel0 implements IWeapon{
-    get armorPenetrationCoeff(): number {return 1};
-    get barrelLength(): number {return 47};
-    get damageCoeff(): number {return 1};
-    get movementSpeedCoeff(): number {return 1};
-    get reloadSpeed(): number {return 3000};
+class WeaponModel0 implements IWeapon {
+    get armorPenetrationCoeff(): number { return 1 }
+    get barrelLength(): number { return 47 }
+    get damageCoeff(): number { return 1 }
+    get movementSpeedCoeff(): number { return 1 }
+    get reloadSpeed(): number { return 3000 }
 }
