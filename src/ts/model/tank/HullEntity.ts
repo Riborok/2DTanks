25c0dd45b3a8f8c4c2f8d3925b8700fdc9db188b
Ferret <stas.gabrus@gmail.com>
2023-08-15 17:57:04 +0300

    added stats for standard parts of tank

--- src/ts/model/tank/HullEntity.ts
+++ src/ts/model/tank/HullEntity.ts
@@ -7,9 +7,17 @@ export abstract class HullEntity extends RectangularEntity {
     }
     protected abstract _health: number;
     protected abstract _armor: number;
-    protected abstract _armorStrength: number; // 0 to 1
+    protected _armorStrength: number = 1; // 0 to 1
+    public get health(){return this._health};
+    public get armor(){return this._armor};
+    public get armorStrength(){return this._armorStrength};
     public takeDamage(bullet: BulletEntity) {
         this._armorStrength -= bullet.armorPenetration;
         this._health -= (bullet.damage - this._armor * this._armorStrength);
     }
+}
+
+class HullModel0 extends HullEntity{
+    protected _armor: number = 100;
+    protected _health: number = 100;
 }
--- src/ts/model/tank/ITrack.ts
+++ src/ts/model/tank/ITrack.ts
@@ -2,3 +2,8 @@ export interface ITrack {
     get movementSpeed(): number;
     get angleSpeed(): number;
 }
+
+class TrackModel0 implements ITrack{
+    get angleSpeed(): number {return 0.05}
+    get movementSpeed(): number {return 3}
+}
