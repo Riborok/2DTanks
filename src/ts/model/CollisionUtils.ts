80229c3416119cfa3b0112f2b7d3101e153b6bcb
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-08-16 23:46:55 +0300

    added cleaning empty QuadtreeNode and CollisionUtils

---
+++ src/ts/model/CollisionUtils.ts
@@ -0,0 +1,31 @@
+import {RectangularEntity} from "./IEntity";
+import {Point} from "./Point";
+
+export class CollisionUtils {
+    private constructor() { }
+    public static calculateCollisionVector(movableRectangle: RectangularEntity, immovableRectangle: RectangularEntity):
+            {dx: number, dy: number} {
+        // НЕ РАБОТАЕТ :(
+        return {dx: 0, dy: 0}
+    }
+    public static isCross(rectangle1: RectangularEntity, rectangle2: RectangularEntity): boolean {
+        for (const point of rectangle1.points)
+            if (CollisionUtils.isPointInsideRect(point, rectangle2))
+                return true;
+
+        for (const point of rectangle2.points)
+            if (CollisionUtils.isPointInsideRect(point, rectangle1))
+                return true;
+
+        return false;
+    }
+    private static isPointInsideRect(point: Point, rectangle: RectangularEntity): boolean {
+        return (
+            ((rectangle.points[0].x <= point.x && point.x <= rectangle.points[1].x) &&
+                (rectangle.points[0].y <= point.y && point.y <= rectangle.points[2].y))
+            ||
+            ((rectangle.points[1].x <= point.x && point.x <= rectangle.points[3].x) &&
+                (rectangle.points[1].y <= point.y && point.y <= rectangle.points[4].y))
+        );
+    }
+}
--- src/ts/model/IRectangularEntityStorage.ts
+++ src/ts/model/IRectangularEntityStorage.ts
@@ -1,4 +1,5 @@
 import {RectangularEntity} from "./IEntity";
+import {CollisionUtils} from "./CollisionUtils";
 
 export interface IRectangularEntityStorage {
     insert(rectangularEntity: RectangularEntity): void;
@@ -27,7 +28,8 @@ export class Quadtree implements IRectangularEntityStorage{
 }
 
 class QuadtreeNode {
-    private static readonly CAPACITY: number = 6;
+    private static readonly CAPACITY: number = 8;
+    private static readonly HALF_CAPACITY: number = QuadtreeNode.CAPACITY >> 1;
 
     private _rectangularEntities: Map<number, RectangularEntity> = new Map();
     private _children: QuadtreeNode[] = [];
@@ -36,7 +38,6 @@ class QuadtreeNode {
     public constructor(boundary: { x: number, y: number, width: number, height: number }) {
         this._boundary = boundary;
     }
-
     private subdivide() {
         const x = this._boundary.x;
         const y = this._boundary.y;
@@ -50,7 +51,6 @@ class QuadtreeNode {
 
         this.redistribute();
     }
-
     private redistribute() {
         const remainingRectangularEntities: Map<number, RectangularEntity> = new Map();
 
@@ -68,7 +68,6 @@ class QuadtreeNode {
         }
         this._rectangularEntities = remainingRectangularEntities;
     }
-
     public insert(rectangularEntity: RectangularEntity): void {
         if (this._children.length > 0) {
             for (const child of this._children) {
@@ -82,12 +81,19 @@ class QuadtreeNode {
         if (this._children.length === 0 && this._rectangularEntities.size > QuadtreeNode.CAPACITY)
             this.subdivide();
     }
-
     public remove(rectangularEntity: RectangularEntity) {
         if (this._children.length > 0) {
             for (const child of this._children) {
                 if (child.isContains(rectangularEntity)) {
                     child.remove(rectangularEntity);
+
+                    let totalChildCount = 0;
+                    for (const child of this._children)
+                        totalChildCount += child.getRectangularEntitiesCount();
+
+                    if (totalChildCount <= QuadtreeNode.HALF_CAPACITY)
+                        this.mergeWithChildren();
+
                     return;
                 }
             }
@@ -95,7 +101,6 @@ class QuadtreeNode {
 
         this._rectangularEntities.delete(rectangularEntity.id);
     }
-
     public checkIntersection(rectangularEntity: RectangularEntity): RectangularEntity | null {
         if (!this.isContains(rectangularEntity))
             return null;
@@ -107,19 +112,28 @@ class QuadtreeNode {
         }
 
         for (const [id, otherRectangularEntity] of this._rectangularEntities)
-            if (otherRectangularEntity !== rectangularEntity && this.isCross(rectangularEntity, otherRectangularEntity))
+            if (otherRectangularEntity !== rectangularEntity &&
+                    CollisionUtils.isCross(rectangularEntity, otherRectangularEntity))
                 return otherRectangularEntity;
 
         return null;
     }
+    private mergeWithChildren() {
+        for (const child of this._children) {
+            if (child._children.length !== 0)
+                child.mergeWithChildren();
+            for (const [key, value] of child._rectangularEntities)
+                this._rectangularEntities.set(key, value);
+        }
 
-    private isCross(rectangle1: RectangularEntity, rectangle2: RectangularEntity): boolean {
-        return rectangle1.points[0].x <= rectangle2.points[1].x &&
-            rectangle1.points[1].x >= rectangle2.points[0].x &&
-            rectangle1.points[0].y >= rectangle2.points[2].y &&
-            rectangle1.points[2].y <= rectangle2.points[0].y;
+        this._children = [];
     }
-
+    private getRectangularEntitiesCount(): number {
+        let count = this._rectangularEntities.size;
+        for (const child of this._children)
+            count += child.getRectangularEntitiesCount();
+        return count;
+    }
     private isContains(rectangularEntity: RectangularEntity): boolean {
         return rectangularEntity.points[0].x >= this._boundary.x &&
             rectangularEntity.points[0].x <= this._boundary.x + this._boundary.width &&
