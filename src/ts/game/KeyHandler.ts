24e8e07b6a1fb3556078803575d6071bc82868e8
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-08-20 17:44:36 +0300

    improved project structure

--- src/ts/game/KeyHandler.ts
+++ src/ts/game/KeyHandler.ts
@@ -1,75 +1,145 @@
-import {VK_A, VK_D, VK_DOWN, VK_LEFT, VK_RIGHT, VK_S, VK_UP, VK_W} from "../constants";
-import {IMovementManager} from "./IMovementManager";
-import {TankElement} from "./TankElement";
+import {
+    VK_A,
+    VK_B,
+    VK_C,
+    VK_CTRL,
+    VK_D,
+    VK_DOWN,
+    VK_E,
+    VK_F,
+    VK_G,
+    VK_H,
+    VK_I,
+    VK_J,
+    VK_K,
+    VK_L,
+    VK_LEFT,
+    VK_M,
+    VK_N,
+    VK_O,
+    VK_P,
+    VK_Q,
+    VK_R,
+    VK_RIGHT,
+    VK_S,
+    VK_SPACE,
+    VK_T,
+    VK_U,
+    VK_UP,
+    VK_V,
+    VK_W,
+    VK_X,
+    VK_Y,
+    VK_Z
+} from "../constants";
 
 export class KeyHandler {
-    private static readonly W_MASK: number = 1 << 0;
-    private static readonly S_MASK: number = 1 << 1;
-    private static readonly D_MASK: number = 1 << 2;
-    private static readonly A_MASK: number = 1 << 3;
+    public static readonly A_MASK: number = 1 << 0;
+    public static readonly B_MASK: number = 1 << 1;
+    public static readonly C_MASK: number = 1 << 2;
+    public static readonly D_MASK: number = 1 << 3;
+    public static readonly E_MASK: number = 1 << 4;
+    public static readonly F_MASK: number = 1 << 5;
+    public static readonly G_MASK: number = 1 << 6;
+    public static readonly H_MASK: number = 1 << 7;
+    public static readonly I_MASK: number = 1 << 8;
+    public static readonly J_MASK: number = 1 << 9;
+    public static readonly K_MASK: number = 1 << 10;
+    public static readonly L_MASK: number = 1 << 11;
+    public static readonly M_MASK: number = 1 << 12;
+    public static readonly N_MASK: number = 1 << 13;
+    public static readonly O_MASK: number = 1 << 14;
+    public static readonly P_MASK: number = 1 << 15;
+    public static readonly Q_MASK: number = 1 << 16;
+    public static readonly R_MASK: number = 1 << 17;
+    public static readonly S_MASK: number = 1 << 18;
+    public static readonly T_MASK: number = 1 << 19;
+    public static readonly U_MASK: number = 1 << 20;
+    public static readonly V_MASK: number = 1 << 21;
+    public static readonly W_MASK: number = 1 << 22;
+    public static readonly X_MASK: number = 1 << 23;
+    public static readonly Y_MASK: number = 1 << 24;
+    public static readonly Z_MASK: number = 1 << 25;
 
-    private static readonly UP_MASK: number = 1 << 4;
-    private static readonly DOWN_MASK: number = 1 << 5;
-    private static readonly RIGHT_MASK: number = 1 << 6;
-    private static readonly LEFT_MASK: number = 1 << 7;
+    public static readonly CTRL_MASK: number = 1 << 26;
+    public static readonly SPACE_MASK: number = 1 << 27;
 
+    public static readonly UP_MASK: number = 1 << 28;
+    public static readonly DOWN_MASK: number = 1 << 29;
+    public static readonly RIGHT_MASK: number = 1 << 30;
+    public static readonly LEFT_MASK: number = 1 << 31;
 
     private _keysMask: number = 0;
-    private readonly _tankElements: TankElement[] = [];
-    private readonly _movementManager: IMovementManager;
-    public constructor(movementManager: IMovementManager) {
-        this._movementManager = movementManager;
+    public constructor() {
         document.addEventListener('keydown', this.handleKeyDown.bind(this));
         document.addEventListener('keyup', this.handleKeyUp.bind(this));
     }
     public clearMask() { this._keysMask = 0 }
-    public addTankElement(tankElement: TankElement) {
-        this._tankElements.push(tankElement);
-    }
-    public handleKeys() {
-        if (this._tankElements.length >= 1 && this._tankElements[0] !== null) {
-            if (this._keysMask & KeyHandler.W_MASK)
-                this._movementManager.moveForward(this._tankElements[0]);
-
-            if (this._keysMask & KeyHandler.S_MASK)
-                this._movementManager.moveBackward(this._tankElements[0]);
-
-            if (this._keysMask & KeyHandler.D_MASK)
-                this._movementManager.hullClockwiseMovement(this._tankElements[0]);
-
-            if (this._keysMask & KeyHandler.A_MASK)
-                this._movementManager.hullCounterclockwiseMovement(this._tankElements[0]);
-        }
-        if (this._tankElements.length >= 2 && this._tankElements[1] !== null) {
-            if (this._keysMask & KeyHandler.UP_MASK)
-                this._movementManager.moveForward(this._tankElements[1]);
-
-            if (this._keysMask & KeyHandler.DOWN_MASK)
-                this._movementManager.moveBackward(this._tankElements[1]);
-
-            if (this._keysMask & KeyHandler.RIGHT_MASK)
-                this._movementManager.hullClockwiseMovement(this._tankElements[1]);
-
-            if (this._keysMask & KeyHandler.LEFT_MASK)
-                this._movementManager.hullCounterclockwiseMovement(this._tankElements[1]);
-        }
-    }
+    public get keysMask(): number {return this._keysMask}
     private handleKeyDown(event: KeyboardEvent) {
         this._keysMask |= KeyHandler.getMask(event.keyCode);
     }
     private handleKeyUp(event: KeyboardEvent) {
         this._keysMask &= ~KeyHandler.getMask(event.keyCode);
     }
-    private static getMask(keyCode: number): number {
+    public static getMask(keyCode: number): number {
         switch (keyCode) {
-            case VK_W:
-                return KeyHandler.W_MASK;
-            case VK_S:
-                return KeyHandler.S_MASK;
             case VK_A:
                 return KeyHandler.A_MASK;
+            case VK_B:
+                return KeyHandler.B_MASK;
+            case VK_C:
+                return KeyHandler.C_MASK;
             case VK_D:
                 return KeyHandler.D_MASK;
+            case VK_E:
+                return KeyHandler.E_MASK;
+            case VK_F:
+                return KeyHandler.F_MASK;
+            case VK_G:
+                return KeyHandler.G_MASK;
+            case VK_H:
+                return KeyHandler.H_MASK;
+            case VK_I:
+                return KeyHandler.I_MASK;
+            case VK_J:
+                return KeyHandler.J_MASK;
+            case VK_K:
+                return KeyHandler.K_MASK;
+            case VK_L:
+                return KeyHandler.L_MASK;
+            case VK_M:
+                return KeyHandler.M_MASK;
+            case VK_N:
+                return KeyHandler.N_MASK;
+            case VK_O:
+                return KeyHandler.O_MASK;
+            case VK_P:
+                return KeyHandler.P_MASK;
+            case VK_Q:
+                return KeyHandler.Q_MASK;
+            case VK_R:
+                return KeyHandler.R_MASK;
+            case VK_S:
+                return KeyHandler.S_MASK;
+            case VK_T:
+                return KeyHandler.T_MASK;
+            case VK_U:
+                return KeyHandler.U_MASK;
+            case VK_V:
+                return KeyHandler.V_MASK;
+            case VK_W:
+                return KeyHandler.W_MASK;
+            case VK_X:
+                return KeyHandler.X_MASK;
+            case VK_Y:
+                return KeyHandler.Y_MASK;
+            case VK_Z:
+                return KeyHandler.Z_MASK;
+            case VK_CTRL:
+                return KeyHandler.CTRL_MASK;
+            case VK_SPACE:
+                return KeyHandler.SPACE_MASK;
             case VK_UP:
                 return KeyHandler.UP_MASK;
             case VK_DOWN:
