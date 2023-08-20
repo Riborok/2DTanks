ead676fdbb344995fee02d5669ce77c413b2ade2
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-08-20 13:29:21 +0300

    added opportunity for 2 players

--- src/ts/game/GameMaster.ts
+++ src/ts/game/GameMaster.ts
@@ -6,11 +6,6 @@ import {Field} from "./Field";
 import {IRectangularEntityStorage, Quadtree} from "../model/IRectangularEntityStorage";
 import {IMovementManager, MovementManager} from "./IMovementManager";
 import {TankElement} from "./TankElement";
-import {Tank} from "../model/tank/Tank";
-import {TankParts} from "../model/tank/TankParts";
-import {TankCreator} from "../model/tank/TankCreator";
-import {TankSprite} from "../sprite/TankSprite";
-import {TankSpriteParts} from "../sprite/TankSpriteParts";
 import {KeyHandler} from "./KeyHandler";
 
 export class GameMaster {
@@ -38,28 +33,17 @@ export class GameMaster {
         this._obstacleCreator.createObstaclesAroundPerimeter(MATERIAL[2]);
     }
 
-    // КРИВУЛЬКА
+    // TODO: КРИВУЛЬКА
     public createTank() {
-        const model : Tank = new Tank(
-            new TankParts(
-                TankCreator.createHull(0, this._field.width >> 1, this._field.height >> 1, 0),
-                TankCreator.createTrack(0),
-                TankCreator.createTurret(0),
-                TankCreator.createWeapon(0)
-            )
-        );
-
-        const sprite = new TankSprite(new TankSpriteParts(0, 0, 0, 0, 0));
-
-        const tankElement : TankElement = { model, sprite }
+        let tankElement = new TankElement(this._field.canvas, 400, 400, 0, 0,
+            0, 0, 0, 0);
         this._movementManager.display(tankElement);
-        this._field.canvas.appendChild(sprite.tankSpriteParts.upTrackSprite.sprite);
-        this._field.canvas.appendChild(sprite.tankSpriteParts.downTrackSprite.sprite);
-        this._field.canvas.appendChild(sprite.tankSpriteParts.hullSprite.sprite);
-        this._field.canvas.appendChild(sprite.tankSpriteParts.weaponSprite.sprite);
-        this._field.canvas.appendChild(sprite.tankSpriteParts.turretSprite.sprite);
-
         this._keyHandler.addTankElement(tankElement);
+
+        // tankElement = new TankElement(this._field.canvas, 800, 800, 0, 1,
+        //     0, 0, 0, 0);
+        // this._movementManager.display(tankElement);
+        // this._keyHandler.addTankElement(tankElement);
     }
 
     // Game loop
--- src/ts/game/TankElement.ts
+++ src/ts/game/TankElement.ts
@@ -1,7 +1,32 @@
 import {Tank} from "../model/tank/Tank";
 import {TankSprite} from "../sprite/TankSprite";
+import {TankParts} from "../model/tank/TankParts";
+import {TankCreator} from "../model/tank/TankCreator";
+import {TankSpriteParts} from "../sprite/TankSpriteParts";
 
-export type TankElement = {
-    model: Tank;
-    sprite: TankSprite;
+export class TankElement {
+    private readonly _model: Tank;
+    private readonly _sprite: TankSprite;
+    public get model(): Tank { return this._model }
+    public get sprite(): TankSprite { return this._sprite }
+    public constructor(canvas: Element, x0: number, y0: number, angle: number, color: number,
+                       hullNum: number, trackNum: number, turretNum: number, weaponNum: number) {
+        const tankSpriteParts = new TankSpriteParts(color, hullNum, trackNum, turretNum, weaponNum);
+        canvas.appendChild(tankSpriteParts.upTrackSprite.sprite);
+        canvas.appendChild(tankSpriteParts.downTrackSprite.sprite);
+        canvas.appendChild(tankSpriteParts.hullSprite.sprite);
+        canvas.appendChild(tankSpriteParts.weaponSprite.sprite);
+        canvas.appendChild(tankSpriteParts.turretSprite.sprite);
+
+        this._sprite = new TankSprite(tankSpriteParts);
+
+        this._model = new Tank(
+            new TankParts(
+                TankCreator.createHull(hullNum, x0, y0, angle),
+                TankCreator.createTrack(trackNum),
+                TankCreator.createTurret(turretNum),
+                TankCreator.createWeapon(weaponNum)
+            )
+        );
+    }
 }
