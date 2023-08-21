f6ac84b89fd4bea199de4fd4aba1fea144996b87
Ferret <stas.gabrus@gmail.com>
2023-08-21 17:09:23 +0300

    fixed sprite illustration

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,6 +1,7 @@
 export const MATERIAL: string[] = ['Grass', 'Ground', 'Sandstone'];
-export const HULL_WIDTH: number[] = [84, 90, 84, 71, 90, 90, 84, 77];
-export const HULL_HEIGHT: number[] = [77, 77, 60, 51, 85, 69, 59, 51];
+export const TRACK_INDENT: number = 5;
+export const HULL_WIDTH: number[] = [76, 81, 77, 64, 81, 81, 76, 69];
+export const HULL_HEIGHT: number[] = [49, 62, 49, 41, 69, 56, 48, 41];
 export const TURRET_INDENT_X: number[] = [20, 25, 20, 15, 25, 25, 25, 20];
 export const TURRET_WIDTH: number[] = [43, 50, 36, 36, 29, 36, 43, 29];
 export const TURRET_HEIGHT: number[] = [36, 36, 22, 36, 29, 29, 36, 29];
