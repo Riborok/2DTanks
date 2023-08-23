8a2b6a8649c22ebab1513ded8b6e37b4292c7f8d
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-08-23 18:02:22 +0300

    updated ValueCaching to support generic keys and values

--- src/ts/ValueCaching.ts
+++ src/ts/ValueCaching.ts
@@ -1,18 +1,21 @@
-class ValueCaching {
-    private readonly _cache: Map<number, number> = new Map();
-    private readonly _calcFunc: (index: number) => number;
+class ValueCaching<TKey, TValue> {
+    private readonly _cache: Map<TKey, TValue> = new Map();
+    private readonly _calcFunc: (key: TKey) => TValue;
     private static readonly MAX_CACHE_SIZE: number = 42;
 
-    public constructor(calcFunc: (index: number) => number) { this._calcFunc = calcFunc; }
-    public getValue(index: number): number {
-        if (this._cache.has(index))
-            return this._cache.get(index);
+    public constructor(calcFunc: (key: TKey) => TValue) {
+        this._calcFunc = calcFunc;
+    }
+
+    public getValue(key: TKey): TValue {
+        if (this._cache.has(key))
+            return this._cache.get(key);
 
         if (this._cache.size > ValueCaching.MAX_CACHE_SIZE)
             this._cache.clear();
 
-        const value = this._calcFunc(index);
-        this._cache.set(index, value);
+        const value = this._calcFunc(key);
+        this._cache.set(key, value);
 
         return value;
     }
@@ -20,20 +23,12 @@ class ValueCaching {
 
 export class SinCache {
     private constructor() {}
-
-    private static readonly SIN_CACHE: ValueCaching = new ValueCaching(Math.sin);
-
-    public static getSin(angle: number): number {
-        return SinCache.SIN_CACHE.getValue(angle);
-    }
+    private static readonly SIN_CACHE: ValueCaching<number, number> = new ValueCaching<number, number>(Math.sin);
+    public static getSin(angle: number): number { return SinCache.SIN_CACHE.getValue(angle) }
 }
 
 export class CosCache {
     private constructor() {}
-
-    private static readonly COS_CACHE: ValueCaching = new ValueCaching(Math.cos);
-
-    public static getCos(angle: number): number {
-        return CosCache.COS_CACHE.getValue(angle);
-    }
+    private static readonly COS_CACHE: ValueCaching<number, number> = new ValueCaching<number, number>(Math.cos);
+    public static getCos(angle: number): number { return CosCache.COS_CACHE.getValue(angle) }
 }
