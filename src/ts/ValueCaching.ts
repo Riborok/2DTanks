c78c774be487037495dc664d17b26a32944172f7
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-08-23 18:53:34 +0300

    minor updates

--- src/ts/ValueCaching.ts
+++ src/ts/ValueCaching.ts
@@ -1,17 +1,18 @@
-class ValueCaching<TKey, TValue> {
+export class ValueCaching<TKey, TValue> {
     private readonly _cache: Map<TKey, TValue> = new Map();
     private readonly _calcFunc: (key: TKey) => TValue;
-    private static readonly MAX_CACHE_SIZE: number = 42;
+    private readonly _maxCacheSize: number;
 
-    public constructor(calcFunc: (key: TKey) => TValue) {
+    public constructor(calcFunc: (key: TKey) => TValue, maxCacheSize: number) {
         this._calcFunc = calcFunc;
+        this._maxCacheSize = maxCacheSize;
     }
 
     public getValue(key: TKey): TValue {
         if (this._cache.has(key))
             return this._cache.get(key);
 
-        if (this._cache.size > ValueCaching.MAX_CACHE_SIZE)
+        if (this._cache.size > this._maxCacheSize)
             this._cache.clear();
 
         const value = this._calcFunc(key);
@@ -23,12 +24,12 @@ class ValueCaching<TKey, TValue> {
 
 export class SinCache {
     private constructor() {}
-    private static readonly SIN_CACHE: ValueCaching<number, number> = new ValueCaching<number, number>(Math.sin);
+    private static readonly SIN_CACHE: ValueCaching<number, number> = new ValueCaching<number, number>(Math.sin, 42);
     public static getSin(angle: number): number { return SinCache.SIN_CACHE.getValue(angle) }
 }
 
 export class CosCache {
     private constructor() {}
-    private static readonly COS_CACHE: ValueCaching<number, number> = new ValueCaching<number, number>(Math.cos);
+    private static readonly COS_CACHE: ValueCaching<number, number> = new ValueCaching<number, number>(Math.cos, 42);
     public static getCos(angle: number): number { return CosCache.COS_CACHE.getValue(angle) }
 }
