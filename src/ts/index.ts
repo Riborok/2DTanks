b52ed35b19aa18c4f48a44396bc221ec80aa89d7
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-08-25 23:21:38 +0300

    minor updates

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -9,11 +9,11 @@ const gameMaster : IGameMaster = new GameMaster(
 
 gameMaster.createField(1,2);
 gameMaster.addTankElements(
-    new TankElement(400, 400, 0, 0,
+    new TankElement(300, 300, 0, 0,
         0, 0, 0, 0,
         KeyHandler.W_MASK, KeyHandler.S_MASK, KeyHandler.D_MASK, KeyHandler.A_MASK,
         KeyHandler.E_MASK, KeyHandler.Q_MASK),
-    new TankElement(800, 800, 0, 1,
+    new TankElement(450, 450, 0, 1,
         0, 0, 0, 0,
         KeyHandler.UP_MASK, KeyHandler.DOWN_MASK, KeyHandler.RIGHT_MASK, KeyHandler.LEFT_MASK,
         KeyHandler.PERIOD_MASK, KeyHandler.COMMA_MASK)
