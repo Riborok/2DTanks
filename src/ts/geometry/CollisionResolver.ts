ce38745e93fa55d41bbb6c604726f7d388047835
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-09-09 16:10:31 +0300

    added change in angular velocity after collision

--- src/ts/geometry/CollisionResolver.ts
+++ src/ts/geometry/CollisionResolver.ts
@@ -6,7 +6,7 @@ import {CollisionDetector} from "./CollisionDetector";
 export class CollisionResolver {
     private constructor() {}
     private static readonly coefficientOfRestitution: number = 0.6;
-    private static readonly CORRECTION_FACTOR: number = 1.5;
+    private static readonly CORRECTION_FACTOR: number = 3;
     public static resolveCollision(impartingEntity: IEntity, receivingEntity: IEntity) {
         const collisionResult = CollisionDetector.getCollisionResult(impartingEntity, receivingEntity);
         if (collisionResult === null)
@@ -14,16 +14,27 @@ export class CollisionResolver {
 
         const collisionNormal = this.calcCollisionNormal(collisionResult.collisionPoint,
             impartingEntity.calcCenter());
+        const impulseMagnitude = this.calsImpulseMagnitude(impartingEntity, receivingEntity, collisionNormal);
 
         this.separateEntities(impartingEntity, collisionResult.overlap, collisionNormal);
-        this.updateVelocity(impartingEntity, receivingEntity, collisionNormal);
+        this.updateVelocity(impartingEntity, receivingEntity, impulseMagnitude, collisionNormal);
+        this.updateAngularVelocity(impartingEntity, receivingEntity, collisionResult.collisionPoint, impulseMagnitude, collisionNormal);
     }
-    private static updateVelocity(impartingEntity: IEntity, receivingEntity: IEntity, collisionNormal: Vector) {
-        const relativeVelocity = VectorUtils.subtract(impartingEntity.velocity, receivingEntity.velocity);
+    private static updateAngularVelocity(impartingEntity: IEntity, receivingEntity: IEntity, collisionPoint: Point,
+                                         impulseMagnitude: number, collisionNormal: Vector) {
+        const radiusReceiving = VectorUtils.subtract(collisionPoint, receivingEntity.calcCenter());
+        const radiusImparting = VectorUtils.subtract(collisionPoint, impartingEntity.calcCenter());
 
-        const impulseMagnitude = VectorUtils.dotProduct(relativeVelocity, collisionNormal) * 2 /
-            (1 / impartingEntity.mass + 1 / receivingEntity.mass) * this.coefficientOfRestitution;
+        const torqueReceiving = VectorUtils.crossProduct(radiusReceiving , collisionNormal) * impulseMagnitude;
 
+        radiusReceiving.normalize();
+        const torqueImparting = VectorUtils.crossProduct(radiusImparting , radiusReceiving) * impulseMagnitude;
+
+        impartingEntity.angularVelocity += torqueImparting  / impartingEntity.momentOfInertia;
+        receivingEntity.angularVelocity += torqueReceiving / receivingEntity.momentOfInertia;
+    }
+    private static updateVelocity(impartingEntity: IEntity, receivingEntity: IEntity, impulseMagnitude: number,
+                                  collisionNormal: Vector) {
         let newImpulse = VectorUtils.scale(collisionNormal, -impulseMagnitude / impartingEntity.mass);
         impartingEntity.velocity.addVector(newImpulse);
 
@@ -35,6 +46,12 @@ export class CollisionResolver {
         const correctionY = -collisionNormal.y * overlap * this.CORRECTION_FACTOR;
         for (const point of impartingEntity.points)
             point.addToCoordinates(correctionX, correctionY);
+    }
+    private static calsImpulseMagnitude(impartingEntity: IEntity, receivingEntity: IEntity, collisionNormal: Vector) {
+        const relativeVelocity = VectorUtils.subtract(impartingEntity.velocity, receivingEntity.velocity);
+
+        return  VectorUtils.dotProduct(relativeVelocity, collisionNormal) * 2 /
+            (1 / impartingEntity.mass + 1 / receivingEntity.mass) * this.coefficientOfRestitution;
     }
     private static calcCollisionNormal(collisionPoint: Point, center: Point): Vector {
         const collisionNormal = VectorUtils.subtract(collisionPoint, center);
