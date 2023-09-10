d6a304a96b7178da56fa0c8abd6e6f9d95f96284
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-09-10 15:26:08 +0300

    improved structure IDTracker

--- src/ts/game/id/IDTracker.ts
+++ src/ts/game/id/IDTracker.ts
@@ -1,20 +1,61 @@
+/**
+ * The IDTracker class manages the unique identifiers for different game entities.
+ * It ensures that each entity is assigned a unique ID within its category.
+ */
 export class IDTracker {
+    // Private constructor to prevent instantiation
     private constructor() { }
 
+    // Range of IDs for tanks
     public static readonly STARTING_TANK_ID: number = 0;
-    public static readonly ENDING_TANK_ID: number = IDTracker.STARTING_TANK_ID + 420_000;
+    public static readonly ENDING_TANK_ID: number = IDTracker.STARTING_TANK_ID + 42_42_42_42_42_42_42;
 
+    // Range of IDs for walls
     public static readonly STARTING_WALL_ID: number = IDTracker.ENDING_TANK_ID + 1;
-    public static readonly ENDING_WALL_ID: number = IDTracker.STARTING_WALL_ID + 420_000;
+    public static readonly ENDING_WALL_ID: number = IDTracker.STARTING_WALL_ID + 42_42_42_42_42_42_42;
 
+    // Range of IDs for bullets
     public static readonly STARTING_BULLET_ID: number = IDTracker.ENDING_WALL_ID + 1;
-    public static readonly ENDING_BULLET_ID: number = IDTracker.STARTING_BULLET_ID + 420_000;
+    public static readonly ENDING_BULLET_ID: number = IDTracker.STARTING_BULLET_ID + 42_42_42_42_42_42_42;
 
+    // Current IDs for each entity type
     private static TANK_ID: number = IDTracker.STARTING_TANK_ID;
     private static WALL_ID: number = IDTracker.STARTING_WALL_ID;
     private static BULLET_ID: number = IDTracker.STARTING_BULLET_ID;
 
-    public static get tankId(): number { return IDTracker.TANK_ID++ }
-    public static get wallId(): number { return IDTracker.WALL_ID++ }
-    public static get bulletId(): number { return IDTracker.BULLET_ID++ }
+    /**
+     * Get the next available tank ID and increment the counter.
+     * Throws an error if the maximum ID is reached.
+     * @returns The unique ID for a tank.
+     */
+    public static get tankId(): number {
+        if (IDTracker.TANK_ID > IDTracker.ENDING_TANK_ID)
+            throw new Error("Maximum tank ID is reached");
+
+        return IDTracker.TANK_ID++;
+    }
+
+    /**
+     * Get the next available wall ID and increment the counter.
+     * Throws an error if the maximum ID is reached.
+     * @returns The unique ID for a wall.
+     */
+    public static get wallId(): number {
+        if (IDTracker.WALL_ID > IDTracker.ENDING_WALL_ID)
+            throw new Error("Maximum wall ID is reached");
+
+        return IDTracker.WALL_ID++;
+    }
+
+    /**
+     * Get the next available bullet ID and increment the counter.
+     * Throws an error if the maximum ID is reached.
+     * @returns The unique ID for a bullet.
+     */
+    public static get bulletId(): number {
+        if (IDTracker.BULLET_ID > IDTracker.ENDING_BULLET_ID)
+            throw new Error("Maximum bullet ID is reached");
+
+        return IDTracker.BULLET_ID++;
+    }
 }
