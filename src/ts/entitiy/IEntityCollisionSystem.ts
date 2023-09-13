0d60986360285033e48bd36291a52b1044c85c0c
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-09-13 19:25:41 +0300

    fixed bugs in QuadtreeNode

--- src/ts/entitiy/IEntityCollisionSystem.ts
+++ src/ts/entitiy/IEntityCollisionSystem.ts
@@ -22,7 +22,7 @@ export class Quadtree implements IEntityCollisionSystem{
     private readonly _boundary: Boundary;
     public constructor(xStart: number, yStart: number, xLast: number, yLast: number) {
         this._boundary = { xStart, yStart, xLast, yLast };
-        this._root = new QuadtreeNode(this._boundary, null);
+        this._root = new QuadtreeNode(this._boundary);
     }
     public insert(entity: IEntity) {
         this._root.insert(entity);
@@ -34,7 +34,7 @@ export class Quadtree implements IEntityCollisionSystem{
         this._root.remove(entity);
     }
     public clear() {
-        this._root = new QuadtreeNode(this._boundary, null);
+        this._root = new QuadtreeNode(this._boundary);
     }
 }
 
@@ -43,18 +43,16 @@ class QuadtreeNode {
     private static readonly HALF_CAPACITY: number = QuadtreeNode.CAPACITY >> 1;
 
     // The private variable _totalEntities represents the total amount of entities within a node.
-    // However, it's important to note that this count may not be equivalent to the amount of entities in the node
+    // However,this count may not be equivalent to the amount of entities in the node
     // if it's divided. Due to the possibility of entities being stored in multiple quadrants,
     // this count may be higher than the actual number of distinct entities.
     private _totalEntities: number = 0;
     private _entities: Map<number, IEntity> | null = new Map();
     private _children: QuadtreeNode[] | null = null;
 
-    private readonly _parent: QuadtreeNode | null;
     private readonly _boundary: Boundary;
-    public constructor(boundary: Boundary, parent: QuadtreeNode | null) {
+    public constructor(boundary: Boundary) {
         this._boundary = boundary;
-        this._parent = parent;
     }
     private isSubdivide(): boolean { return this._entities === null }
     private subdivide() {
@@ -67,47 +65,57 @@ class QuadtreeNode {
         const height = (yLast - yStart) / 2;
 
         this._children = [
-            new QuadtreeNode({ xStart, yStart, xLast: xStart + width, yLast: yStart + height }, this),
-            new QuadtreeNode({ xStart: xStart + width, yStart, xLast, yLast: yStart + height }, this),
-            new QuadtreeNode({ xStart, yStart: yStart + height, xLast: xStart + width, yLast }, this),
-            new QuadtreeNode({ xStart: xStart + width, yStart: yStart + height, xLast, yLast }, this)
+            new QuadtreeNode({ xStart, yStart, xLast: xStart + width, yLast: yStart + height }),
+            new QuadtreeNode({ xStart: xStart + width, yStart, xLast, yLast: yStart + height }),
+            new QuadtreeNode({ xStart, yStart: yStart + height, xLast: xStart + width, yLast }),
+            new QuadtreeNode({ xStart: xStart + width, yStart: yStart + height, xLast, yLast })
         ];
 
         this.redistribute();
     }
     private redistribute() {
-        for (const entity of this._entities.values())
-            for (const child of this._children)
+        this._totalEntities = 0;
+        for (const child of this._children) {
+            for (const entity of this._entities.values())
                 if (child.isContainsEntity(entity))
                     child.insert(entity);
+            this._totalEntities += child._totalEntities;
+        }
 
         this._entities = null;
     }
     public insert(entity: IEntity) {
-        this._totalEntities++;
         if (this.isSubdivide()) {
-            for (const child of this._children)
+            this._totalEntities = 0;
+            for (const child of this._children) {
                 if (child.isContainsEntity(entity))
                     child.insert(entity);
+
+                this._totalEntities += child._totalEntities;
+            }
         }
         else {
+            this._totalEntities++;
             this._entities.set(entity.id, entity);
             if (this._entities.size > QuadtreeNode.CAPACITY)
                 this.subdivide();
         }
     }
     public remove(entity: IEntity) {
-        this._totalEntities--;
         if (this.isSubdivide()) {
-            for (const child of this._children)
+            this._totalEntities = 0;
+            for (const child of this._children) {
                 if (child.isContainsEntity(entity))
                     child.remove(entity);
-            this.mergeCheck();
+
+                this._totalEntities += child._totalEntities;
+            }
+            if (this._totalEntities <= QuadtreeNode.HALF_CAPACITY)
+                this.mergeWithChildren();
         }
         else {
+            this._totalEntities--;
             this._entities.delete(entity.id);
-            if (this._parent !== null)
-                this._parent.mergeCheck();
         }
     }
     public getCollisions(entity: IEntity): IEntity[] {
@@ -137,10 +145,6 @@ class QuadtreeNode {
         return point.x > this._boundary.xStart && point.x < this._boundary.xLast &&
             point.y > this._boundary.yStart && point.y < this._boundary.yLast;
     }
-    private mergeCheck() {
-        if (this._totalEntities <= QuadtreeNode.HALF_CAPACITY)
-            this.mergeWithChildren();
-    }
     private mergeWithChildren() {
         this._entities = new Map<number, IEntity>();
         for (const child of this._children) {
@@ -149,6 +153,7 @@ class QuadtreeNode {
             for (const entity of child._entities.values())
                 this._entities.set(entity.id, entity);
         }
+        this._totalEntities = this._entities.size;
 
         this._children = null;
     }
