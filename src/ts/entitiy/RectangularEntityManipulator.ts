5fada6e2c27407e2e0598ef237bf375b9a3bc17c
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-09-17 18:27:25 +0300

    fixed a bug with bullet movement

--- src/ts/entitiy/RectangularEntityManipulator.ts
+++ src/ts/entitiy/RectangularEntityManipulator.ts
@@ -1,4 +1,5 @@
 import {IEntity} from "./IEntity";
+import {Vector} from "../geometry/Point";
 
 /**
  * Class provides utility methods for manipulating rectangular entities.
@@ -7,21 +8,23 @@ export class RectangularEntityManipulator {
     private constructor() { }
 
     /**
-     * Move the front part of a rectangular entity based on its current velocity.
+     * Move the front part of a rectangular entity based on velocity.
      * The front part includes points 1 and 2.
-     * @param entity The rectangular entity to move.
+     * @param entity - The rectangular entity to move.
+     * @param velocity - The velocity vector to apply to the front part.
      */
-    public static movementFront(entity: IEntity) {
-        entity.points[1].addToCoordinates(entity.velocity.x, entity.velocity.y);
-        entity.points[2].addToCoordinates(entity.velocity.x, entity.velocity.y);
+    public static movementFront(entity: IEntity, velocity: Vector) {
+        entity.points[1].addToCoordinates(velocity.x, velocity.y);
+        entity.points[2].addToCoordinates(velocity.x, velocity.y);
     }
     /**
-     * Move the back part of a rectangular entity based on its current velocity.
+     * Move the back part of a rectangular entity based on velocity.
      * The back part includes points 0 and 3.
-     * @param entity The rectangular entity to move.
+     * @param entity - The rectangular entity to move.
+     * @param velocity - The velocity vector to apply to the back part.
      */
-    public static movementBack(entity: IEntity) {
-        entity.points[0].addToCoordinates(entity.velocity.x, entity.velocity.y);
-        entity.points[3].addToCoordinates(entity.velocity.x, entity.velocity.y);
+    public static movementBack(entity: IEntity, velocity: Vector) {
+        entity.points[0].addToCoordinates(velocity.x, velocity.y);
+        entity.points[3].addToCoordinates(velocity.x, velocity.y);
     }
 }
