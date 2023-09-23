a149ec667d57b34cd8dcd211f0781de6a52ac339
Egor Pankratiev <egor.pankratiew@gmail.com>
2023-09-23 22:53:32 +0300

    added IEventEmitter

--- src/ts/game/IKeyHandler.ts
+++ src/ts/game/IKeyHandler.ts
@@ -1,19 +1,29 @@
-export interface IKeyHandler {
+import {IEventEmitter} from "../additionally/type";
+
+export interface IKeyHandler extends IEventEmitter {
     clearKeys(): void;
     isKeyDown(keyCode: number): boolean;
 }
 
 export class KeyHandler implements IKeyHandler {
     private readonly _keys: Set<number> = new Set();
+    private readonly keyDownHandler: (event: KeyboardEvent) => void;
+    private readonly keyUpHandler: (event: KeyboardEvent) => void;
     public constructor() {
-        document.addEventListener('keydown', this.handleKeyDown.bind(this));
-        document.addEventListener('keyup', this.handleKeyUp.bind(this));
+        this.keyDownHandler = this.handleKeyDown.bind(this);
+        this.keyUpHandler = this.handleKeyUp.bind(this);
+        document.addEventListener('keydown', this.keyDownHandler);
+        document.addEventListener('keyup', this.keyUpHandler);
     }
     public clearKeys() {
         this._keys.clear();
     }
     public isKeyDown(keyCode: number): boolean {
         return this._keys.has(keyCode);
+    }
+    public removeEventListeners() {
+        document.removeEventListener('keydown', this.keyDownHandler);
+        document.removeEventListener('keyup', this.keyUpHandler);
     }
     private handleKeyDown(event: KeyboardEvent) {
         this._keys.add(event.keyCode);
