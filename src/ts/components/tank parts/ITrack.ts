2f6a4cfc3f0b8192694d48f465dd4d76f0dc9ac4
Ferret451 <stas.gabrus@gmail.com>
2023-10-06 18:30:08 +0300

    reworked some tank part stats

--- src/ts/components/tank parts/ITrack.ts
+++ src/ts/components/tank parts/ITrack.ts
@@ -8,7 +8,7 @@ export interface ITrack extends IComponent{
 }
 
 export class TrackModel0 implements ITrack{
-    private static readonly ANGULAR_DATA: MotionData = { finishSpeed: 0.0175, force: 0.03 }
+    private static readonly ANGULAR_DATA: MotionData = { finishSpeed: 0.02, force: 0.03 }
     private static readonly FORWARD_DATA: MotionData = { finishSpeed: 3, force: 0.03 }
     private static readonly BACKWARD_DATA: MotionData = { finishSpeed: 2, force: 0.025 }
     private static readonly NUM: number = 0;
@@ -21,8 +21,8 @@ export class TrackModel0 implements ITrack{
 
 export class TrackModel1 implements ITrack{
     private static readonly ANGULAR_DATA: MotionData = { finishSpeed: 0.019, force: 0.02 }
-    private static readonly FORWARD_DATA: MotionData = { finishSpeed: 3.5, force: 0.02 }
-    private static readonly BACKWARD_DATA: MotionData = { finishSpeed: 2.5, force: 0.015 }
+    private static readonly FORWARD_DATA: MotionData = { finishSpeed: 3.5, force: 0.03 }
+    private static readonly BACKWARD_DATA: MotionData = { finishSpeed: 2.5, force: 0.025 }
     private static readonly NUM: number = 1;
 
     public angularData: MotionData = { ...TrackModel1.ANGULAR_DATA }
@@ -32,9 +32,9 @@ export class TrackModel1 implements ITrack{
 }
 
 export class TrackModel2 implements ITrack{
-    private static readonly ANGULAR_DATA: MotionData = { finishSpeed: 0.014, force: 0.015 }
-    private static readonly FORWARD_DATA: MotionData = { finishSpeed: 4, force: 0.035 }
-    private static readonly BACKWARD_DATA: MotionData = { finishSpeed: 3, force: 0.03 }
+    private static readonly ANGULAR_DATA: MotionData = { finishSpeed: 0.02, force: 0.02 }
+    private static readonly FORWARD_DATA: MotionData = { finishSpeed: 2, force: 0.04 }
+    private static readonly BACKWARD_DATA: MotionData = { finishSpeed: 1, force: 0.035 }
     private static readonly NUM: number = 2;
 
     public angularData: MotionData = { ...TrackModel2.ANGULAR_DATA }
@@ -45,8 +45,8 @@ export class TrackModel2 implements ITrack{
 
 export class TrackModel3 implements ITrack{
     private static readonly ANGULAR_DATA: MotionData = { finishSpeed: 0.03, force: 0.04 }
-    private static readonly FORWARD_DATA: MotionData = { finishSpeed: 2, force: 0.025 }
-    private static readonly BACKWARD_DATA: MotionData = { finishSpeed: 1, force: 0.02 }
+    private static readonly FORWARD_DATA: MotionData = { finishSpeed: 2, force: 0.015 }
+    private static readonly BACKWARD_DATA: MotionData = { finishSpeed: 1, force: 0.01 }
     private static readonly NUM: number = 3;
 
     public angularData: MotionData = { ...TrackModel3.ANGULAR_DATA }
