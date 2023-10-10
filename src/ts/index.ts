d23c666cf061eb53d96a8bf818341aca4e811eca
Egor Pankratiev <112758007+Riborok@users.noreply.github.com>
2023-10-10 20:26:27 +0300

    removed DOMContentLoaded event handler and added defer attribute to the script

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,16 +1,5 @@
 import {ITankChooseManager, TankChooseManager} from "./game/managers/additional/ITankChooseManager";
 import {GameModeManager} from "./game/game mode/GameModeManager";
 
-
-const onloadListener = () => {
-    const buttons = document.querySelectorAll("button");
-
-    const tankChooseManager: ITankChooseManager = new TankChooseManager();
-
-    tankChooseManager.start(GameModeManager.gameStart, buttons);
-
-    document.removeEventListener("DOMContentLoaded", onloadListener);
-};
-document.addEventListener("DOMContentLoaded", onloadListener);
-
-
+const tankChooseManager: ITankChooseManager = new TankChooseManager();
+tankChooseManager.start(GameModeManager.gameStart, document.querySelectorAll("button"));
