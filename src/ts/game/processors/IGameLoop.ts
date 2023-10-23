5557157f19fd0ebfaa9c97492db9e546e25b9f14
Egor Pankratiev <112758007+Riborok@users.noreply.github.com>
2023-10-23 16:50:29 +0300

    changed text color, removed performance.now, fixed bug with pause

--- src/ts/game/processors/IGameLoop.ts
+++ src/ts/game/processors/IGameLoop.ts
@@ -4,37 +4,47 @@ import {IDrawable} from "./ICanvas";
 export interface IGameLoop {
     stop(): void;
     start(): void;
+    pause(): void;
+    resume(): void;
     get render(): IRender;
 }
 
 export class GameLoop implements IGameLoop {
-    private _isGameLoopActive: boolean = false;
+    private _isRunning: boolean = false;
+    private _isActivate: boolean = false;
+
     private _lastFrameTime: number;
     private readonly _render: IRender = new Render();
     private readonly _drawable: IDrawable;
     public constructor(drawable: IDrawable) { this._drawable = drawable }
     public get render(): IRender { return this._render }
     public start() {
-        if (!this._isGameLoopActive) {
-            this._isGameLoopActive = true;
-            this._lastFrameTime = performance.now();
-            requestAnimationFrame(this.gameLoop);
+        if (!this._isRunning) {
+            this._isRunning = true;
+            this.resume();
+            this.gameLoop();
         }
     }
     public stop() {
-        this._isGameLoopActive = false;
+        this._isRunning = false;
+    }
+    public pause() {
+        this._isActivate = false;
+    }
+    public resume() {
+        this._isActivate = true;
+        this._lastFrameTime = Date.now();
     }
     private readonly gameLoop = () => {
-        if (!this._isGameLoopActive)
-            return;
+        if (this._isRunning) {
+            if (this._isActivate) {
+                const currentTime = Date.now();
+                this._render.renderAll(currentTime - this._lastFrameTime);
+                this._drawable.drawAll();
+                this._lastFrameTime = currentTime;
+            }
 
-        const currentTime = performance.now();
-
-        this._render.renderAll(currentTime - this._lastFrameTime);
-        this._drawable.drawAll();
-
-        this._lastFrameTime = currentTime;
-
-        requestAnimationFrame(this.gameLoop);
+            requestAnimationFrame(this.gameLoop);
+        }
     }
 }
