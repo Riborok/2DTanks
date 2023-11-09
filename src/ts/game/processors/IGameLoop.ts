c1360385f335a69828f4ccdd315111a7bb2f8a99
Egor Pankratiev <112758007+Riborok@users.noreply.github.com>
2023-11-09 14:53:09 +0300

    fixed bugs with redrawing

--- src/ts/game/processors/IGameLoop.ts
+++ src/ts/game/processors/IGameLoop.ts
@@ -4,34 +4,47 @@ import {IDrawable} from "./ICanvas";
 export interface IGameLoop {
     stop(): void;
     start(): void;
+    pause(): void;
+    resume(): void;
     get render(): IRender;
 }
 
 export class GameLoop implements IGameLoop {
+    private _isRunning: boolean = false;
+    private _isActivate: boolean = false;
     private _lastFrameTime: number;
-    private _lastCallID: number = 0;
 
     private readonly _render: IRender = new Render();
     private readonly _drawable: IDrawable;
     public constructor(drawable: IDrawable) { this._drawable = drawable }
     public get render(): IRender { return this._render }
     public start() {
-        if (this._lastCallID === 0) {
-            this._lastFrameTime = performance.now();
-            this._lastCallID = requestAnimationFrame(this.gameLoop);
+        if (!this._isRunning) {
+            this._isRunning = true;
+            this.resume();
+            requestAnimationFrame(this.gameLoop);
         }
     }
     public stop() {
-        cancelAnimationFrame(this._lastCallID);
-        this._lastCallID = 0;
+        this._isRunning = false;
+    }
+    public pause() {
+        this._isActivate = false;
+    }
+    public resume() {
+        this._isActivate = true;
+        this._lastFrameTime = performance.now();
     }
     private readonly gameLoop = (highResTimeStamp: DOMHighResTimeStamp) => {
-        const time = highResTimeStamp.valueOf();
+        if (this._isRunning) {
+            if (this._isActivate) {
+                const currentTime = highResTimeStamp.valueOf();
+                this._render.renderAll(currentTime - this._lastFrameTime);
+                this._drawable.drawAll();
+                this._lastFrameTime = currentTime;
+            }
 
-        this._render.renderAll(time - this._lastFrameTime);
-        this._drawable.drawAll();
-        this._lastFrameTime = time;
-
-        this._lastCallID = requestAnimationFrame(this.gameLoop);
+            requestAnimationFrame(this.gameLoop);
+        }
     }
 }
